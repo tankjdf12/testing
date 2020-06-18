3c80f824749f60589ecbc71f6c92b0acd9c0bbaf
tankjdf12 <52118067+tankjdf12@users.noreply.github.com>
2020-06-18 10:58:52 -0400

    Update testing.js

--- src/js/testing.js
+++ src/js/testing.js
@@ -37,36 +37,107 @@
             fname = patient.name[0].given.join(' ');
             lname = patient.name[0].family.join(' ');
           }
-          
+
+          var height = byCodes('8302-2');
+          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
+          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          var hdl = byCodes('2085-9');
+          var ldl = byCodes('2089-1');
+          var heartrate = byCodes('55283-6');
+          var creatinine = byCodes('12190-5');
+
           var p = defaultPatient();
-          
+          p.birthdate = patient.birthDate;
+          p.gender = gender;
           p.fname = fname;
           p.lname = lname;
-                    
+          p.height = getQuantityValueAndUnit(height[0]);
+
+          if (typeof systolicbp != 'undefined')  {
+            p.systolicbp = systolicbp;
+          }
+
+          if (typeof diastolicbp != 'undefined') {
+            p.diastolicbp = diastolicbp;
+          }
+
+          p.hdl = getQuantityValueAndUnit(hdl[0]);
+          p.ldl = getQuantityValueAndUnit(ldl[0]);
+          p.heartrate = getQuantityValueAndUnit(heartrate[0])
+          p.creatinine = getQuantityValueAndUnit(creatinine[0])
+
           ret.resolve(p);
         });
       } else {
         onError();
       }
     }
-    
+
     FHIR.oauth2.ready(onReady, onError);
     return ret.promise();
 
   };
-  
-    function defaultPatient(){
+
+  function defaultPatient(){
     return {
       fname: {value: ''},
       lname: {value: ''},
+      gender: {value: ''},
+      birthdate: {value: ''},
+      height: {value: ''},
+      systolicbp: {value: ''},
+      diastolicbp: {value: ''},
+      ldl: {value: ''},
+      hdl: {value: ''},
+      heartrate: {value: ''},
+      creatinine: {value: ''},
     };
   }
+
+  function getBloodPressureValue(BPObservations, typeOfPressure) {
+    var formattedBPObservations = [];
+    BPObservations.forEach(function(observation){
+      var BP = observation.component.find(function(component){
+        return component.code.coding.find(function(coding) {
+          return coding.code == typeOfPressure;
+        });
+      });
+      if (BP) {
+        observation.valueQuantity = BP.valueQuantity;
+        formattedBPObservations.push(observation);
+      }
+    });
+
+    return getQuantityValueAndUnit(formattedBPObservations[0]);
+  }
+
+  function getQuantityValueAndUnit(ob) {
+    if (typeof ob != 'undefined' &&
+        typeof ob.valueQuantity != 'undefined' &&
+        typeof ob.valueQuantity.value != 'undefined' &&
+        typeof ob.valueQuantity.unit != 'undefined') {
+          return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
+    } else {
+      return undefined;
+    }
+  }
+
   window.drawVisualization = function(p) {
     $('#holder').show();
     $('#loading').hide();
     $('#fname').html(p.fname);
     $('#lname').html(p.lname);
-   
+    $('#gender').html(p.gender);
+    $('#birthdate').html(p.birthdate);
+    $('#height').html(p.height);
+    $('#systolicbp').html(p.systolicbp);
+    $('#diastolicbp').html(p.diastolicbp);
+    $('#ldl').html(p.ldl);
+    $('#hdl').html(p.hdl);
+    $('#heartrate).html(p.heartrate);
+    $('#creatinine).html(p.creatinine);
+
   };
 
 })(window);
+
